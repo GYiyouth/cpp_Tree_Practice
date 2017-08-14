173f75ff99e95cd97c62ef8efa244781c93090e1
geyao <geyao2012@gmail.com>
2017-08-14 23:36:50 +0800

    完成了非递归的各种遍历。

--- Tree.cpp
+++ Tree.cpp
@@ -18,7 +18,7 @@ public:
 //templates can't be virtual
 template <class T>
 T& onPointShow(T& data){
-    cout << "function pointer :" << data << endl;
+    cout << "function pointer : " << data << endl;
 }
 //使用实例
 void treeTest(){
@@ -27,16 +27,61 @@ void treeTest(){
     auto tree = new Tree<int>();
     //根节点
     auto root = new Node<int>(a);
+    //做2个孩子
+    auto b = a + 1 ;
+    auto left = new Node<int>(b);
+    auto c = b + 1 ;
+    auto right = new Node<int>(c);
+    root->setLeftPoint(left);
+    root->setRightPoint(right);
+    auto ll = 4;
+    auto lr = 5;
+    auto rl = 6;
+    auto rr = 7;
+    auto l1 = new Node<int>(ll);
+    auto l2 = new Node<int>(lr);
+    auto r1 = new Node<int>(rl);
+    auto r2 = new Node<int>(rr);
+    left->setLeftPoint(l1);
+    left->setRightPoint(l2);
+    right->setLeftPoint(r1);
+    right->setRightPoint(r2);
+
+
     //继承了回调接口的类
     auto callBackEntity = new CallBackEntity<int>();
     tree->setRoot(root);
-    //设置
+    //设置面向对象的回调
     tree->setCallBack(callBackEntity);
-    //遍历测试
-    tree->preTraversal(tree->getRoot()); //Object call back : 1
     //设置函数指针
     tree->setFCallBack(onPointShow);
-    //再次遍历测试
+    cout << "Pre Recursive function" << endl;
+    //递归前序遍历测试
     tree->preTraversal(tree->getRoot());
+
+    cout << endl;
+
+    cout << "Pre Non-Recursive function" << endl;
+    tree->preTraversal();
+
+    cout << endl;
+
+    cout << "Mid Non-Recursive function" << endl;
+    tree->midTraversal();
+
+    cout << endl;
+
+    cout << "Post Non-Recursive function" << endl;
+    tree->postTraversal();
+
+    cout << endl;
+
+    cout << "Level easy Non-Recursive function" <<endl;
+    tree->levelEasyTraversal();
+
+    cout << endl;
+
+    cout << "Level Non-Recursive function" <<endl;
+    tree->levelTraversal();
 }
 
--- Tree.h
+++ Tree.h
@@ -6,6 +6,8 @@
 #ifndef TREE_TREE_H
 #define TREE_TREE_H
 #include <iostream>
+#include <queue>
+#include <stack>
 #include "Node.h"
 #include "CallBack.h"
 
@@ -20,29 +22,185 @@ private:
     TCallBack<T>* callBack = nullptr;
     //回调函数
     T& (* fCallBack)(T& data);
+
+    //遍历的逻辑
+    inline void doSomeThing(Node<T>* node){
+        if (callBack != nullptr)
+            callBack->onShowTime(node->getDataNode());
+        if (fCallBack!= nullptr)
+            fCallBack(node->getDataNode());
+    }
 protected:
 public:
 
-    //前序遍历,递归版本
+
+
+
+
+    //Pre Recursive version
     virtual void preTraversal(Node<T>* node){
         if (node != nullptr){
-            if (callBack != nullptr)
-                callBack->onShowTime(node->getDataNode());
-            if (fCallBack!= nullptr)
-                fCallBack(node->getDataNode());
+            doSomeThing(node);
             preTraversal(node->getLeftPoint());
             preTraversal(node->getRightPoint());
         } else{
             return;
         }
     }
+
+    //Mid Recursive version
+    virtual void midTraversal(Node<T>* node){
+        if (node != nullptr){
+            midTraversal(node->getLeftPoint());
+            doSomeThing(node);
+            midTraversal(node->getRightPoint());
+        }
+    }
+
+    //Post Recursive version
+    virtual void behindTraversal(Node<T>* node){
+        if (node != nullptr){
+            behindTraversal(node->getLeftPoint());
+            behindTraversal(node->getRightPoint());
+            doSomeThing(node);
+        }
+    }
+
+    //Pre Non-Recursive version
+    virtual void preTraversal(void){
+        //stack of std
+        stack<Node<T>* > myStack; // reference cannot be null, use pointer!
+        Node<T>* node = getRoot();
+        //traversal
+        while(node != nullptr || !myStack.empty()){
+            //only push left child to prevent dead cycle
+            while (node != nullptr){
+                doSomeThing(node);
+                myStack.push(node);
+                node = node->getLeftPoint();
+            }
+            //get Node and visit
+            if (!myStack.empty()){
+                node = myStack.top();
+                myStack.pop();
+                node = node->getRightPoint();
+            }
+        }
+    }
+
+
+    //Mid Non-Recursive version
+    virtual void midTraversal(void){
+        //stack of std
+        stack<Node<T>* > myStack; // reference cannot be null, use pointer!
+        Node<T>* node = getRoot();
+        //traversal
+        while(node != nullptr || !myStack.empty()){
+            //only push left child to prevent dead cycle
+            while (node != nullptr){
+                myStack.push(node);
+                node = node->getLeftPoint();
+            }
+            //get Node and visit
+            if (!myStack.empty()){
+                node = myStack.top();
+                doSomeThing(node);
+                myStack.pop();
+                node = node->getRightPoint();
+            }
+        }
+    }
+
+    //Post Non-Recursive version
+    virtual void postTraversal(void){
+        //stack of std
+        stack<Node<T>* > myStack; // reference cannot be null, use pointer!
+        Node<T>* node = getRoot();
+        //keep the lastVisit node
+        Node<T>* lastVisit = getRoot();
+        //traversal
+        while(node != nullptr || !myStack.empty()){
+            //only push left child to prevent dead cycle
+            while (node != nullptr){
+                myStack.push(node);
+                node = node->getLeftPoint();
+            }
+            //get Node and visit
+            if (!myStack.empty()){
+                node = myStack.top();
+                if (node->getRightPoint() == nullptr || node->getRightPoint() == lastVisit){
+                    doSomeThing(node);
+                    myStack.pop();
+                    lastVisit = node;
+                    node = nullptr;
+                } else{
+                    node = node->getRightPoint();
+                }
+            }
+        }
+    }
+
+    //Level easy Non-Recursive version
+    virtual void levelEasyTraversal(void){
+        //queue of std
+        queue< Node<T> >  myQueue;
+        if (getRoot() != nullptr){
+            myQueue.push(* getRoot()); // this function receive reference var
+        }
+        while (!myQueue.empty()){
+            //get the first node
+            Node<T> node = myQueue.front();
+            //pop
+            myQueue.pop();
+            //my business logic, call back
+            doSomeThing(&node);
+            //deal with children
+            if (node.getLeftPoint() != nullptr){
+                myQueue.push(* node.getLeftPoint());
+            }
+            if (node.getRightPoint() != nullptr){
+                myQueue.push(* node.getRightPoint());
+            }
+        }
+    }
+
+    //Level Non-Recursive version
+    virtual void levelTraversal(void){
+        if (getRoot() == nullptr){
+            return;
+        }
+        queue<Node<T>*> myQueue;
+        myQueue.push(getRoot());
+        myQueue.push(0); // nullptr
+
+        do{
+            Node<T>* node = myQueue.front();
+            myQueue.pop();
+            if (node){ // node != nullptr
+                doSomeThing(node);
+                if (node->getLeftPoint() != nullptr){
+                    myQueue.push( node->getLeftPoint());
+                }
+                if (node->getRightPoint() != nullptr){
+                    myQueue.push(node->getRightPoint());
+                }
+            } else{
+                if (!myQueue.empty()){
+                    //end the the level
+                    myQueue.push(0);
+                    cout << endl;
+                }
+            }
+        }while (!myQueue.empty());
+    }
+
     //设置面向对象的回调成员变量
     void setCallBack(TCallBack<T> *callBack) {
         this -> callBack = callBack;
     }
     //设置面向过程的回调函数
-    void setFCallBack(T &(*fCallBack)(T &)) {
-        Tree::fCallBack = fCallBack;
+    void setFCallBack( T &(*back)(T &) ) {
+        Tree::fCallBack = back;
     }
 
     virtual Node<T> *getRoot() const {
